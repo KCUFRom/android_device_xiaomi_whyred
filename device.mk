08156ede60317d132c965ee6d012b34d7db6364e
Shekhawat2 <anandsingh215@yahoo.com>
2018-06-24 15:09:02 +0530

    whyred: build android.hidl.manager@1.0-java
    *PackageManager: updateAllSharedLibrariesLPw failed: Package com.qualcomm.qcrilmsgtunnel requires unavailable shared library android.hidl.manager@1.0-java; failing!

--- device.mk
+++ device.mk
@@ -251,7 +251,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0 \
-#    android.hidl.manager@1.0-java
+    android.hidl.manager@1.0-java
 
 # IDC
 PRODUCT_COPY_FILES += \
