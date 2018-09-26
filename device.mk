40b571eab277e663072fc166499820c551004fea
Shekhawat2 <anandsingh215@yahoo.com>
2018-09-26 06:51:01 +0000

    whyred: gps: update from caf
    tag:"LA.UM.7.2.r1-04900-sdm660.0"

--- device.mk
+++ device.mk
@@ -190,7 +190,8 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl-qti \
+    android.hardware.gnss@1.1-impl-qti \
+    android.hardware.gnss@1.1-service-qti \
     gps.sdm660 \
     libcurl \
     libgnss \
